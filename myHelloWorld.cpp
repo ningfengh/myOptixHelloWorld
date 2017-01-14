89bfb56dd1c6ce8d0b81b2c59110928d045a2194
Ningfeng Huang <ningfengh@gmail.com>
2017-01-13 22:15:27 -0800

    Diffuse shader and path tracer now there is now light, only light is "environment" this "enironmental light" is implemented by a miss program there is no important sampling for light
    only diffuse brdf lobe is sampled

--- myHelloWorld.cpp
+++ myHelloWorld.cpp
@@ -72,6 +72,10 @@ uint32_t       height = 768u;
 bool           use_pbo = true;
 optix::Aabb    aabb;
 
+int            frame_number = 1;
+int            sqrt_num_samples = 50;
+int            rr_begin_depth = 1;
+
 // Camera state
 float3         camera_up;
 float3         camera_lookat;
@@ -158,22 +162,25 @@ void createContext()
     context = Context::create();
     context->setRayTypeCount( 2 );
     context->setEntryPointCount( 1 );
+    context->setStackSize( 1800 );
 
     context["radiance_ray_type"]->setUint( 0u );
     context["shadow_ray_type"  ]->setUint( 1u );
     context["scene_epsilon"    ]->setFloat( 1.e-4f );
 
-    Buffer buffer = sutil::createOutputBuffer( context, RT_FORMAT_UNSIGNED_BYTE4, width, height, use_pbo );
+    Buffer buffer = sutil::createOutputBuffer( context, RT_FORMAT_FLOAT4, width, height, use_pbo );
     context["output_buffer"]->set( buffer );
 
     // Ray generation program
     std::string ptx_path( ptxPath( "pinhole_camera.cu" ) );
-    Program ray_gen_program = context->createProgramFromPTXFile( ptx_path, "pinhole_camera" );
+    Program ray_gen_program = context->createProgramFromPTXFile( ptx_path, "pathtrace_camera");
     context->setRayGenerationProgram( 0, ray_gen_program );
 
     // Exception program
     Program exception_program = context->createProgramFromPTXFile( ptx_path, "exception" );
     context->setExceptionProgram( 0, exception_program );
+
+    context[ "sqrt_num_samples" ]->setUint( sqrt_num_samples );
     context["bad_color"]->setFloat( 1.0f, 0.0f, 1.0f );
 
     // Miss program
@@ -195,10 +202,10 @@ void loadMesh( const std::string& filename )
     // Create Material and assign it to geometry_group
     std::string ptx_path( ptxPath( "pinhole_camera.cu" ) );
     Material matl = context->createMaterial();
-    Program shade = context->createProgramFromPTXFile(ptx_path, "closest_hit_radiance1");
+    Program shade = context->createProgramFromPTXFile(ptx_path, "diffuse");
     matl->setClosestHitProgram(0, shade);
-    matl["Ka"]->setFloat( 0.3f, 0.0f, 0.0f );
-    matl["Kd"]->setFloat( 0.9f, 0.9f, 0.9f );
+    matl["diffuse_color"]->setFloat( 0.5f, 0.5f, 0.5f );
+
 
 
     OptiXMesh mesh;
